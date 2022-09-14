197b1a979a6e84bda6e80e7514ea2236a0a10149
enspour <enspour@gmail.com>
2022-09-14 22:15:05 +0300

    setting sequence of loaders

--- backend/auth-service/src/app.ts
+++ backend/auth-service/src/app.ts
@@ -1,1 +1,23 @@
-console.log("hello world!");
+import { loadKeysAccessJWT, loadKeysRefreshJWT } from "@loaders/jwt.keys";
+import createApp from "@loaders/app";
+import createServer from "@loaders/http";
+import connectToDataBase from "@loaders/db";
+
+import logger from "@logger";
+
+import serverConfig from "@configs/server.config";
+
+(async () => {
+    await loadKeysAccessJWT();
+    await loadKeysRefreshJWT();
+
+    await connectToDataBase();
+    
+    const app = createApp();
+    const server = createServer(app);
+
+    const port = serverConfig.port;
+
+    server.listen(port, () => logger.log(`Server has been started on port: ${port}`));
+})();
+
