570c69ee25e17b14bf3b7128619321d1c5094273
enspour <enspour@gmail.com>
2022-09-15 18:22:10 +0300

    added answer if route is not found

--- backend/auth-service/src/routes/useRoutes.ts
+++ backend/auth-service/src/routes/useRoutes.ts
@@ -1,11 +1,16 @@
 import { Router, Application } from "express";
 
+import NotFound from "core/server-responses/responses/NotFound.response";
+
 import authRoute from "./auth/auth.route";
 
 const useRoutes = (app: Application) => {
     const router = Router();
     
     router.use("/api/v1/auth-service", authRoute);
+
+    router.use("*", (req, res) => 
+        new NotFound(`URL: ${req.originalUrl} is not found.`).send(res));
 
     app.use(router);
 }
