63350273c0fce5e69cd3b4a9befad9c2e3ca796e
enspour <enspour@gmail.com>
2022-09-15 16:42:43 +0300

    added controllers

---
+++ backend/auth-service/src/controllers/Auth.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express"
+
+export default class AuthController {
+    async login(req: Request, res: Response) {
+
+    }
+
+    async signup(req: Request, res: Response) {
+
+    }
+
+    async logout(req: Request, res: Response) {
+
+    }
+
+    async refresh(req: Request, res: Response) {
+
+    }
+}
---
+++ backend/auth-service/src/controllers/index.ts
@@ -0,0 +1,5 @@
+import AuthController from "./Auth.controller";
+
+export default {
+    auth: new AuthController(),
+}
