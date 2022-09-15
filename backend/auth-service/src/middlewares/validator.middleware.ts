e16a10032fa44bd2bde6f3ca4b10597aa8eea417
enspour <enspour@gmail.com>
2022-09-15 18:21:36 +0300

    updated middleware with server responses

--- backend/auth-service/src/middlewares/validator.middleware.ts
+++ backend/auth-service/src/middlewares/validator.middleware.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 
+import BadRequest from "core/server-responses/responses/BadRequest.response"
+
 const validatorMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        const body = errors.array().map(item => ({
+            name: item.param,
+            message: item.msg
+        }))
+
+        return new BadRequest("Bad request", body).send(res);
     }
 
     next();
