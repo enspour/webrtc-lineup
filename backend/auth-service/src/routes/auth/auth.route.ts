31a416df6f0c708da6606974ccd84ee7086a1976
enspour <enspour@gmail.com>
2022-09-15 18:22:47 +0300

    added wrapper on methods of controller

--- backend/auth-service/src/routes/auth/auth.route.ts
+++ backend/auth-service/src/routes/auth/auth.route.ts
@@ -7,11 +7,13 @@ import signupValidator from "./validators/signup.validator";
 
 import validatorMiddleware from "@middlewares/validator.middleware";
 
+import asyncHandler from "core/server-responses/AsyncHandler";
+
 const authRoute = Router();
 
-authRoute.post("/login", loginValidator, validatorMiddleware, controllers.auth.login);
-authRoute.post("/signup", signupValidator, validatorMiddleware, controllers.auth.signup);
-authRoute.post("/logout", controllers.auth.logout);
-authRoute.post("/refresh", controllers.auth.refresh);
+authRoute.post("/login", loginValidator, validatorMiddleware, asyncHandler(controllers.auth.login));
+authRoute.post("/signup", signupValidator, validatorMiddleware, asyncHandler(controllers.auth.signup));
+authRoute.post("/logout", asyncHandler(controllers.auth.logout));
+authRoute.post("/refresh", asyncHandler(controllers.auth.refresh));
 
 export default authRoute;
