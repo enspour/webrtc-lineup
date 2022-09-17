fc2af5d2ae986841fd71bf15f4eaa5bbcd969c0c
enspour <enspour@gmail.com>
2022-09-17 18:40:10 +0300

    updated returned value in createUser method

--- backend/core/database/src/Repositories/PrismaRepository.ts
+++ backend/core/database/src/Repositories/PrismaRepository.ts
@@ -3,6 +3,7 @@ import { PrismaClient, User, UserAuth } from "@prisma/client";
 import { IRepository } from "./Repository";
 
 import DuplicateError from "../QueryError/DuplicateError";
+import UnknowError from "../QueryError/UnknowError";
 
 export default class PrismaRepository implements IRepository {
     constructor(private prismaClient: PrismaClient) {}
@@ -15,7 +16,7 @@ export default class PrismaRepository implements IRepository {
         })
     }
 
-    async createUser(name: string, email: string, password: string): Promise<User> {
+    async createUser(name: string, email: string, password: string): Promise<User & { email: string }> {
         const foundedUser = await this.prismaClient.userAuth.findUnique({
             where: { email }
         });
@@ -34,6 +35,18 @@ export default class PrismaRepository implements IRepository {
             include: { auth: true }
         });
 
-        return user;
+        if (user.auth) {
+            const clone = {
+                id: user.id,
+                name: user.name,
+                created_at: user.created_at,
+                modified_at: user.modified_at,
+                email: user.auth.email
+            }; 
+
+            return clone;
+        }
+        
+        throw new UnknowError("Error creating user");
     }
 }
--- backend/core/database/src/Repositories/Repository.ts
+++ backend/core/database/src/Repositories/Repository.ts
@@ -3,7 +3,7 @@ import { User, UserAuth } from "../types"
 export interface IRepository {
     findUserAuthByEmail(email: string): Promise<(UserAuth & { user: User }) | null>;
 
-    createUser(name: string, email: string, password: string): Promise<User>;
+    createUser(name: string, email: string, password: string): Promise<User & { email: string }>;
 }
 
 export default class Repository implements IRepository {
@@ -13,7 +13,7 @@ export default class Repository implements IRepository {
         return await this._repository.findUserAuthByEmail(email); 
     }
 
-    async createUser(name: string, email: string, password: string): Promise<User> {
+    async createUser(name: string, email: string, password: string): Promise<User & { email: string }> {
         return await this._repository.createUser(name, email, password);
     }
 }
