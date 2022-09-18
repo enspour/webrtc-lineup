5d62595d62892185c1013da45a67d21ca5a8b0c8
enspour <enspour@gmail.com>
2022-09-18 15:21:42 +0300

    added component svg

---
+++ client/components/ui/Svg/Svg.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Svg = ({ url, height, width, color, onClick }) => (
+    <span
+        onClick={e => onClick?.call({}, e)} 
+        style={{ 
+            padding: "0.3rem",
+            height: "fit-content"
+        }}
+    >
+        <div 
+            style={{ 
+                WebkitMaskSize: "cover",
+                maskSize: "cover",
+                WebkitMaskImage: `url(${url})`,
+                maskImage: `url(${url})`,
+                backgroundColor: color || `var(--theme-color-icon)`, 
+                width: `${width}rem`,
+                height: `${height}rem`,
+                cursor: onClick ? "pointer" : "auto"
+            }}
+        />
+    </span>
+)
+
+Svg.propTypes = {
+    url: PropTypes.string.isRequired,
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    onClick: PropTypes.func,
+    color: PropTypes.string
+}
+
+export default React.memo(Svg);
