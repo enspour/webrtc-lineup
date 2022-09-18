e1028e7a11df919edb76a619f381e65fc419a181
enspour <enspour@gmail.com>
2022-09-18 18:35:56 +0300

    added checkbox component

---
+++ client/app/components/ui/CheckBox/CheckBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "./CheckBox.module.scss";
+
+const CheckBox = ({ label, setValue }) => {
+    return (
+        <div>
+            <label className={styles.wrapper}>
+                <input type="checkbox" className={styles.input} onClick={e => setValue(e.target.checked)}/>
+                <span className={styles.checkbox}></span>
+                <span className={styles.label}>{label}</span>
+            </label>
+        </div>
+    )
+}
+
+export default CheckBox;
