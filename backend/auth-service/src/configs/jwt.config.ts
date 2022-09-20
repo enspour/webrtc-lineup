6e92c9162e895a805bce7f069bdb3c4d0872b17c
enspour <enspour@gmail.com>
2022-09-20 18:04:58 +0300

    updated expires of access jsonwebtoken

--- backend/auth-service/src/configs/jwt.config.ts
+++ backend/auth-service/src/configs/jwt.config.ts
@@ -4,7 +4,7 @@ export default {
     accessToken: {
         publicKeyPath: path.join(__dirname, "../..", "keys/jwt/access/jwtRS256.key.pub"),
         privateKeyPath: path.join(__dirname, "../..", "keys/jwt/access/jwtRS256.key"),
-        expiresIn: "10m",
+        expiresIn: "20m",
     },
     refreshToken: {
         publicKeyPath: path.join(__dirname, "../..", "keys/jwt/refresh/jwtRS256.key.pub"),
