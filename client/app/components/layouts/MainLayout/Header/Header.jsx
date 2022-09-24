fd76f029990771fd7d6af86243208d3f15ae7a30
enspour <enspour@gmail.com>
2022-09-25 00:07:27 +0300

    added menu and updated styles

--- client/app/components/layouts/MainLayout/Header/Header.jsx
+++ client/app/components/layouts/MainLayout/Header/Header.jsx
@@ -1,24 +1,65 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { observer } from "mobx-react-lite";
+
+import Dropdown from "@components/ui/Dropdown/Dropdown";
 
 import services from "@services";
 
 import styles from "./Header.module.scss";
 
-const Header = observer(() => { 
+const AccountMenu = ({ isOpen }) => {
+    const router = useRouter();
+
+    const gotoAccount = () => {
+        router.push("/account");
+    }
+
+    const logout = async () => {
+        await services.authAPI.logout();
+        router.push("/login");
+    }
+
+    const AccountMenuItems = [
+        { id: 1, name: "Account", onClick: gotoAccount},
+        { id: 2, name: "Logout", onClick: logout}
+    ];
+
+    return <Dropdown isOpen={isOpen} items={AccountMenuItems}/>;
+}
+
+const Account = observer(() => {
     const user = services.user.User;
-    
+
+    const accountRef = React.useRef();
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    React.useEffect(() => {
+        if (isOpen) {
+            accountRef.current.classList.add(styles.account__active);
+        } else {
+            accountRef.current.classList.remove(styles.account__active);
+        }
+    }, [isOpen]);
+
+    return (
+        <div className={styles.account} ref={accountRef} onClick={() => setIsOpen((prev) => !prev)}>
+            <div className={styles.account__avatar}></div>
+            <div> {user.name} </div>
+            <AccountMenu isOpen={isOpen}/>
+        </div>
+    )
+})
+
+const Header = observer(() => { 
     return (
         <header className={styles.header}>
             <Link href="/">
                 <div className={styles.header__logo}>Lineup</div>
             </Link>
 
-            <div className={styles.account}>
-                <div className={styles.account__avatar}></div>
-                <div> {user.name} </div>
-            </div>
+            <Account />
         </header>
     )
 });
