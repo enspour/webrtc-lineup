832eb03c4dd987bbf95012a4cd370192501ee536
enspour <enspour@gmail.com>
2022-09-24 17:34:00 +0300

    removed unnecessary styles

--- client/app/components/ui/SearchInput/SearchInput.jsx
+++ client/app/components/ui/SearchInput/SearchInput.jsx
@@ -2,27 +2,23 @@ import React from "react";
 
 import Svg from "../Svg/Svg";
 
-import SearchSVG from "@assets/images/search/search.svg";
 import ClearSVG from "@assets/images/search/clear.svg";
 
 import styles from "./SearchInput.module.scss";
 
-const SearchInput = ({ placeholder, value, setValue, onClear }) => {
+const SearchInput = ({ placeholder, value, setValue }) => {
     return (
         <div className={styles.search}>
-            <Svg url={SearchSVG} height="1.8" width="1.8"/>
-
             <input
                 className={styles.search__input}
                 type="text"
+                placeholder={placeholder}
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
                 required
             />
-            <div className={styles.border}></div>
-            <span className={styles.placeholder}> {placeholder} </span>
 
-            <Svg url={ClearSVG} height="1.4" width="1.4" onClick={e => onClear(e)}/>
+            <Svg url={ClearSVG} height="1.4" width="1.4" onClick={() => setValue("")}/>
         </div>
     )
 }
