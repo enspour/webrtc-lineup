0da2aa7ef832aca86bdb659ec44c2d12e68f1672
enspour <enspour@gmail.com>
2022-09-25 00:07:56 +0300

    updated index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,19 @@
-const Home = () => {
+import React from "react"; 
+
+import MainLayout from "@components/layouts/MainLayout/MainLayout";
+import TabPanel from "@components/ui/TabPanel/TabPanel";
+
+import styles from "@styles/pages/index.module.scss";
+
+const Home = () => { 
+    const [tab, setTab] = React.useState({ name: "Library" });
+
     return (
-        <div> Hello world </div>
+        <MainLayout> 
+            <div className={styles.tabPanel}>
+                <TabPanel tab={tab} setTab={setTab}/>
+            </div>
+        </MainLayout>
     );
 }
 
