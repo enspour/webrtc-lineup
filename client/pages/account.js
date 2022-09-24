18406ace138abd4cd9e87132e240179ad4db7505
enspour <enspour@gmail.com>
2022-09-25 00:08:09 +0300

    added account page

---
+++ client/pages/account.js
@@ -0,0 +1,11 @@
+import MainLayout from "@components/layouts/MainLayout/MainLayout";
+
+const Account = () => {
+    return (
+        <MainLayout>
+            <div> Account </div>
+        </MainLayout>
+    )
+}
+
+export default Account;
