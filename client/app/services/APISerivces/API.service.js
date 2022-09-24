2d6c079a2172938c596db2ecb63b1176f892ab8b
enspour <enspour@gmail.com>
2022-09-24 19:45:45 +0300

    fixed error

--- client/app/services/APISerivces/API.service.js
+++ client/app/services/APISerivces/API.service.js
@@ -30,7 +30,7 @@ class Request {
             this.count -= 1;
 
             if (this.count === 0) {
-                const isRefreshTokens = this.#tryRefreshTokens(error);
+                const isRefreshTokens = this.#tryRefreshTokens(err);
                 if (isRefreshTokens) {
                     return await this.start(data);
                 }
