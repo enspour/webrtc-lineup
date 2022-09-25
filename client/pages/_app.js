e57681b3ec318c1aadb38a224fb7296fcf81b80f
enspour <enspour@gmail.com>
2022-09-25 18:24:51 +0300

    added loader theme

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,22 @@
+import React from "react";
+
+import useLoaderTheme from "@hooks/loaders/useLoaderTheme";
+
+import services from "@services";
+
 import "@assets/fonts/stylesheet.css";
 import "@styles/reset.scss";
 import "@styles/base.scss";
-import "@styles/variables.scss";
+// import "@styles/variables.scss";
+import "@styles/helpers.scss";
 
 function MyApp({ Component, pageProps }) {
+    useLoaderTheme();
+
+    React.useLayoutEffect(() => {
+        services.initialize();
+    }, []);
+
     return <Component {...pageProps} />
 }
 
