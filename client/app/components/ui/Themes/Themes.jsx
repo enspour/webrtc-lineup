050114c320620d0b672250464062955e56c758cd
enspour <enspour@gmail.com>
2022-09-25 18:23:57 +0300

    added component themes

---
+++ client/app/components/ui/Themes/Themes.jsx
@@ -0,0 +1,46 @@
+import useThemes from "@hooks/useThemes";
+
+import styles from "./Themes.module.scss";
+
+const ThemeItem = ({ active, theme, setTheme }) => {
+    return (
+        <div 
+            className={styles.theme} 
+            onClick={() => setTheme(theme)}
+            style={
+                active 
+                    ? { border: ".2rem solid var(--theme-border-secondary)" } 
+                    : { border: ".2rem solid var(--theme-border-primary)" } 
+            }
+        >
+            <div 
+                className={styles.theme__primary} 
+                style={{ borderLeft: `6rem solid ${theme.theme["--theme-bg-primary"]}` }}
+            />
+            <div 
+                className={styles.theme__secondary} 
+                style={{ borderBottom: `6rem solid ${theme.theme["--theme-bg-tertiary"]}` }}
+            />
+        </div>
+    )
+}
+
+
+const Themes = () => {
+    const [theme, setTheme, themes] = useThemes();
+
+    return (
+        <div className={styles.themes}>
+            { themes.map(item => 
+                <ThemeItem 
+                    key={item.id} 
+                    active={theme.id === item.id} 
+                    theme={item} 
+                    setTheme={setTheme}
+                />
+            ) }
+        </div>
+    )
+}
+
+export default Themes;
