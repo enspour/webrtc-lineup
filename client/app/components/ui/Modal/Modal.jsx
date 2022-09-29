ae132148398982623e4a52364dbae5e446babddc
enspour <enspour@gmail.com>
2022-09-29 17:25:06 +0300

    added close modal when click outside

--- client/app/components/ui/Modal/Modal.jsx
+++ client/app/components/ui/Modal/Modal.jsx
@@ -6,12 +6,20 @@ import useCssAnimation from "@hooks/css/useCssAnimation";
 
 import styles from "./Modal.module.scss";
 
-const Modal = ({ title, visible, setVisible, children }) => {
-    const windowRef = useCssAnimation(styles.visible, visible, [visible]);
+const Modal = ({ title, isOpen, setIsOpen, children }) => {
+    const windowRef = useCssAnimation(styles.visible, isOpen, [isOpen]);
+    
+    const modalRef = React.useRef();
+
+    const closeModal = e => {
+        if (modalRef.current && !modalRef.current.contains(e.target)) {
+            setIsOpen(false);
+        }
+    }
 
     return (
-        <div className={styles.window} ref={windowRef}>
-            <div className={styles.modal}>
+        <div className={styles.window} ref={windowRef} onClick={closeModal}>
+            <div className={styles.modal} ref={modalRef}>
                 <Panel>
                     <div className={styles.wrapper}>
                         <div className={styles.modal__title}>{title}</div>
