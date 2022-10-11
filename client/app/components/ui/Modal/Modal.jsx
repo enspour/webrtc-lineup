6e8069df428020e6baa0d363dfa2e32b15fa42ca
enspour <enspour@gmail.com>
2022-10-11 15:11:21 +0300

    fixed issue: appear modal when height of windows is small

--- client/app/components/ui/Modal/Modal.jsx
+++ client/app/components/ui/Modal/Modal.jsx
@@ -8,19 +8,19 @@ import useCssAnimation from "@hooks/css/useCssAnimation";
 import styles from "./Modal.module.scss";
 
 const Modal = ({ title, isOpen, setIsOpen, children }) => {
-    const windowRef = useCssAnimation(styles.visible, isOpen, [isOpen]);
+    const modalRef = useCssAnimation(styles.visible, isOpen, [isOpen]);
     
-    const modalRef = React.useRef();
+    const modalContentRef = React.useRef();
 
     const closeModal = e => {
-        if (modalRef.current && !modalRef.current.contains(e.target)) {
+        if (modalContentRef.current && !modalContentRef.current.contains(e.target)) {
             setIsOpen(false);
         }
     }
 
     return (
-        <div className={styles.window} ref={windowRef} onClick={closeModal}>
-            <div className={styles.modal} ref={modalRef}>
+        <div className={styles.modal} ref={modalRef} onClick={closeModal}>
+            <div className={styles.modal__content} ref={modalContentRef}>
                 <Panel>
                     <div className={styles.wrapper}>
                         <div className={styles.modal__title}>{title}</div>
