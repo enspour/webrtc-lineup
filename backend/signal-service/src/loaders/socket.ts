46479891359e4558902a3b82539e9b9fdb32d53a
enspour <enspour@gmail.com>
2022-10-15 23:48:49 +0300

    added guard middleware to checkout user is auth

--- backend/signal-service/src/loaders/socket.ts
+++ backend/signal-service/src/loaders/socket.ts
@@ -3,6 +3,8 @@ import { Server } from "socket.io"
 
 import initIO from "@socket/io";
 import initEvents from "@socket/events";
+import initActions from "@socket/actions";
+import initMiddlewares from "@socket/middlewares";
 
 const createSocket = (server: http.Server) => {
     const io = new Server(server, {
@@ -11,7 +13,9 @@ const createSocket = (server: http.Server) => {
     });
 
     initIO(io);
+    initMiddlewares(io);
     initEvents(io);
+    initActions(io);
 
     return io;
 }
