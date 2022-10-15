7e1e7b2383237f6e9cfb0df4f9c8125c402eae6f
enspour <enspour@gmail.com>
2022-10-15 23:50:11 +0300

    updated actions of socket

--- backend/signal-service/src/socket/actions/index.ts
+++ backend/signal-service/src/socket/actions/index.ts
@@ -1,1 +1,17 @@
-export const Actions = {}
+import { Server, Socket } from "socket.io"
+
+import { onJoinRoom, onLeaveRoom } from "./room.actions";
+
+export const Actions = {
+    JOIN_ROOM: "room:join",
+    LEAVE_ROOM: "room:leave",
+}
+
+const initActions = (io: Server) => {
+    io.on("connection", (socket: Socket) => {
+        socket.on(Actions.JOIN_ROOM, onJoinRoom)
+        socket.on(Actions.LEAVE_ROOM, onLeaveRoom)
+    })
+}
+
+export default initActions;
