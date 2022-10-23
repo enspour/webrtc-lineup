9519bcd5208a5820316085441adf47b50844e3eb
enspour <enspour@gmail.com>
2022-10-23 18:36:03 +0300

    changed styles

--- client/app/components/modals/RoomModal/RoomModal.jsx
+++ client/app/components/modals/RoomModal/RoomModal.jsx
@@ -21,7 +21,7 @@ const Information = observer(() => {
     const owner = services.modals.room.Owner;
 
     return (
-        <div className="w-55">
+        <div className="w-60">
             <div className={styles.room__name}>{name}</div>
             <div className={styles.room__owner__name}>{owner.name}</div>
         </div>
