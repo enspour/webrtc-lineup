8f324c86565c17465e8c343bb811b6a3bb8f93d8
enspour <enspour@gmail.com>
2022-10-23 23:05:52 +0300

    recoding removeDuplicates. more performance

--- client/app/utils/removeDuplicates.js
+++ client/app/utils/removeDuplicates.js
@@ -1,6 +1,17 @@
 const removeDuplicates = (arr) => {
-    const set = new Set(arr);
-    return [...set]
+    const unique = [];
+    const map = {};
+
+    for (const item of arr) {
+        if (map[item]) {
+            continue;
+        }
+
+        map[item] = true;
+        unique.push(item);
+    }
+
+    return unique;
 }
 
 export default removeDuplicates;
