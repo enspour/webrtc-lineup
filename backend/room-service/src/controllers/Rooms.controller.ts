ea62d9a7faeae7908452c1315c2535c4b0f53a43
enspour <enspour@gmail.com>
2022-10-26 18:10:31 +0300

    refactoring

--- backend/room-service/src/controllers/Rooms.controller.ts
+++ backend/room-service/src/controllers/Rooms.controller.ts
@@ -18,17 +18,7 @@ class RoomsController {
 
         const room = await RoomsService.create(name, password, user.id, tags);
 
-        new SuccessResponse({
-            ...room,
-            id: room.id.toString(),
-            owner_id: room.owner_id.toString(),
-            tags: room.tags.map(item => 
-                ({
-                    ...item,
-                    id: item.id.toString()
-                })
-            )
-        }).send(res);
+        new SuccessResponse({ room }).send(res);
     }
 
     async delete(req: Request, res: Response) {
@@ -39,11 +29,7 @@ class RoomsController {
         const room = await RoomsService.delete(id, user.id);
 
         if (room) { 
-            return new SuccessResponse({
-                ...room,
-                id: room.id.toString(),
-                owner_id: room.owner_id.toString(),
-            }).send(res);
+            return new SuccessResponse({ room }).send(res);
         }
 
         new NotFoundResponse("Room is not found.").send(res);
@@ -52,17 +38,7 @@ class RoomsController {
     async getCreatedRooms(req: Request, res: Response) {
         const user = getUser(req);
 
-        const list = await RoomsService.getCreated(user.id);
-
-        const rooms = list.map(item => ({
-            ...item,
-            id: item.id.toString(),
-            owner_id: item.owner_id.toString(),
-            tags: item.tags.map(tag => ({
-                id: tag.id.toString(),
-                name: tag.name
-            }))
-        }))
+        const rooms = await RoomsService.getCreated(user.id);
 
         new SuccessResponse({ rooms }).send(res);
     }
@@ -70,23 +46,7 @@ class RoomsController {
     async getFavoritesRooms(req: Request, res: Response) {
         const user = getUser(req);
 
-        const list = await RoomsService.getFavorites(user.id);
-
-        const rooms = list.map(item => ({
-            id: item.id.toString(),
-            name: item.name,
-            status: item.status,
-            created_at: item.created_at,
-            owner_id: item.owner_id.toString(),
-            owner: {
-                ...item.owner,
-                id: item.owner.id.toString()
-            },
-            tags: item.tags.map(tag => ({
-                id: tag.id.toString(),
-                name: tag.name,
-            }))
-        }))
+        const rooms = await RoomsService.getFavorites(user.id);
 
         new SuccessResponse({ rooms }).send(res);
     }
@@ -98,13 +58,7 @@ class RoomsController {
         const room = await RoomsService.addToFavorites(id, user.id);
 
         if (room) {
-            return new SuccessResponse({
-                room: { 
-                    ...room, 
-                    id: room.id.toString(), 
-                    owner_id: room.owner_id.toString() 
-                }
-            }).send(res);
+            return new SuccessResponse({ room }).send(res);
         }
 
         new BadRequestResponse("Room is not found.").send(res);
@@ -117,13 +71,7 @@ class RoomsController {
         const room = await RoomsService.deleteFromFavorites(id, user.id);
 
         if (room) {
-            return new SuccessResponse({ 
-                room: { 
-                    ...room, 
-                    id: room.id.toString(), 
-                    owner_id: room.owner_id.toString() 
-                } 
-            }).send(res);
+            return new SuccessResponse({ room }).send(res);
         }
 
         new BadRequestResponse("Room is not found.").send(res);
@@ -135,21 +83,7 @@ class RoomsController {
 
         const rooms = await RoomsService.search(words, tags);
 
-        new SuccessResponse({
-            rooms: rooms.map(item => ({
-                ...item,
-                id: item.id.toString(),
-                owner_id: item.owner_id.toString(),
-                owner: {
-                    ...item.owner,
-                    id: item.owner.id.toString()
-                },
-                tags: item.tags.map(tag => ({
-                    ...tag,
-                    id: tag.id.toString()
-                }))
-            }))
-        }).send(res);
+        new SuccessResponse({ rooms }).send(res);
     }
 }
 
