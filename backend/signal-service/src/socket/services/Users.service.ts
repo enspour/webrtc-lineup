2f630d98332b8be56299cdb5a7e94979495a0b4b
enspour <enspour@gmail.com>
2022-11-20 01:27:58 +0300

    added method to updating conference information for users

--- backend/signal-service/src/socket/services/Users.service.ts
+++ backend/signal-service/src/socket/services/Users.service.ts
@@ -1,11 +1,11 @@
-import { User, Room, RoomSettings } from "core/database/src/types"
+import { User, Room, RoomSettings, Conference, ConferenceSettings } from "core/database/src/types"
 
 import BroadcastAll from "@socket/notifications/BroadcastAll.notification"
 
 import { RoomActionsTypes } from "@socket/types"
 
 export default class UsersService {
-    updateRoomInformation(id: string, room: (Room & { owner: User, settings: RoomSettings })) {
+    updateRoomInformation(roomId: string, room: (Room & { owner: User, settings: RoomSettings })) {
         const data = 
             JSON.parse(JSON.stringify(
                 room,
@@ -14,6 +14,21 @@ export default class UsersService {
                     : value
             ));
 
-        new BroadcastAll(RoomActionsTypes.NOTIFY_UPDATE_ROOM_INFORMATION, { room: data }).notify(id);
+        new BroadcastAll(RoomActionsTypes.NOTIFY_UPDATE_ROOM_INFORMATION, { room: data }).notify(roomId);
+    }
+
+    updateConferenceInformation(roomId: string, conference: (Conference & { settings: ConferenceSettings })) {
+        const data = 
+            JSON.parse(JSON.stringify(
+                conference,
+                (_, value) => typeof value === "bigint" 
+                    ? value.toString() 
+                    : value
+            ));
+
+        new BroadcastAll(
+            RoomActionsTypes.NOTIFY_UPDATE_CONFERENCE_INFORMATION, 
+            { conference: data }
+        ).notify(roomId);
     }
 }
