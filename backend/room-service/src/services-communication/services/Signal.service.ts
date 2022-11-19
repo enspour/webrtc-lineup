5b88beda04cda1ecde3b8bc339409df93eb26c8d
enspour <enspour@gmail.com>
2022-11-20 01:26:18 +0300

    added updating conference information

--- backend/room-service/src/services-communication/services/Signal.service.ts
+++ backend/room-service/src/services-communication/services/Signal.service.ts
@@ -3,12 +3,25 @@ import fetch from "node-fetch";
 import serverConfig from "@configs/server.config";
 
 class SignalService {
-    async updateRoomInformationConnectedUsers(id: bigint) {
+    async updateRoomInformation(roomId: bigint) {
         try {
             const { backend } = serverConfig;
-            const url = `${backend}/services-communication/signal-service/users/update-room-inforamtion/${id}`;
+            const url = `${backend}/services-communication/signal-service/users/update-room-inforamtion/${roomId}`;
             
             const response = await fetch(url, { method: "PUT" });
+        } catch {}
+    }
+
+    async updateConferenceInformation(roomId: string, conferenceId: string) {
+        try {
+            const { backend } = serverConfig;
+            const url = `${backend}/services-communication/signal-service/users/update-conference-information`;
+
+            const response = await fetch(url, { 
+                method: "PUT",
+                body: JSON.stringify({ room_id: roomId, conference_id: conferenceId }),
+                headers: { "Content-Type": "application/json" }
+            });
         } catch {}
     }
 }
