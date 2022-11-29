1f0bc26aa9d8ab4180599c88e0760507d3e4ff68
enspour <enspour@gmail.com>
2022-11-29 15:59:25 +0300

    added Toolkits

--- client/app/features/room/components/pages/RoomPage/RoomPage.jsx
+++ client/app/features/room/components/pages/RoomPage/RoomPage.jsx
@@ -4,24 +4,49 @@ import { observer } from "mobx-react-lite";
 import RoomLayout from "../../layouts/RoomLayout/RoomLayout";
 
 import ConferenceCard from "../../ui/ConferenceCard/ConferenceCard";
+import ToolkitCard from "../../ui/ToolkitCard/ToolkitCard";
 
 import services from "@services";
 
 import styles from "./RoomPage.module.scss";
 
-const Room = observer(() => {
+const Toolkits = () => {
+    return (
+        <div className={styles.section__items}>
+            <ToolkitCard 
+                title="File Storage" 
+                hint="You can exchange files to conduct productive meetings here."
+            />
+        </div>
+    );
+}
+
+const Conferences = observer(() => {
     const conferences = services.room.ConferencesInfo.Array;
 
     return (
+        <div className={styles.section__items}>
+            { conferences.map(item => <ConferenceCard key={item.id} conference={item}/>) }
+        </div>
+    )
+});
+
+const RoomPage = () => {
+    return (
         <RoomLayout>
-            <div className={styles.conferences}>
-                <div className={styles.conferences__title}> Conferences </div>
-                <div className={styles.conferences__items}>
-                    { conferences.map(item => <ConferenceCard key={item.id} conference={item}/>) }
+            <div className={styles.sections}>
+                <div className={styles.section}>
+                    <div className={styles.section__title}> Conversation </div>
+                    <Conferences />
+                </div>
+                
+                <div className={styles.section}>
+                    <div className={styles.section__title}> Toolkit </div>
+                    <Toolkits />
                 </div>
             </div>
         </RoomLayout>
     )
-});
+}
 
-export default Room;
+export default React.memo(RoomPage);
