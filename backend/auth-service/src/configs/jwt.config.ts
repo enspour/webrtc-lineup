334ea3038636602178129032aa57aa49a316a1f2
enspour <enspour@gmail.com>
2022-12-05 00:47:11 +0300

    fixed bugs after first installing

--- backend/auth-service/src/configs/jwt.config.ts
+++ backend/auth-service/src/configs/jwt.config.ts
@@ -2,13 +2,13 @@ import path from "node:path";
 
 export default {
     accessToken: {
-        publicKeyPath: path.join(__dirname, "../..", "keys/jwt/access/jwtRS256.key.pub"),
-        privateKeyPath: path.join(__dirname, "../..", "keys/jwt/access/jwtRS256.key"),
+        publicKeyPath: path.join(__dirname, "../..", "keys/accessToken/jwtRS256.key.pub"),
+        privateKeyPath: path.join(__dirname, "../..", "keys/accessToken/jwtRS256.key"),
         expiresIn: "20m",
     },
     refreshToken: {
-        publicKeyPath: path.join(__dirname, "../..", "keys/jwt/refresh/jwtRS256.key.pub"),
-        privateKeyPath: path.join(__dirname, "../..", "keys/jwt/refresh/jwtRS256.key"),
+        publicKeyPath: path.join(__dirname, "../..", "keys/refreshToken/jwtRS256.key.pub"),
+        privateKeyPath: path.join(__dirname, "../..", "keys/refreshToken/jwtRS256.key"),
         expiresIn: "7d",
     }
 }
