0a8ff2f8b32adb8593386d3991ad7317afcb28f5
enspour <enspour@gmail.com>
2022-12-11 16:25:23 +0300

    added route to communicate between services for getting user info

--- backend/auth-service/src/services-communication/routes/index.ts
+++ backend/auth-service/src/services-communication/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import authRoutes from "./auth/auth.route";
+import userRoutes from "./user/user.route";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
 
 export default router;
