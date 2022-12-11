e103aec8545b8d2e6359bb1c360b9df3454dbcdb
enspour <enspour@gmail.com>
2022-12-11 16:27:28 +0300

    added queries to comunicate with services

--- backend/signal-service/src/services-communication/services/Room.service.ts
+++ backend/signal-service/src/services-communication/services/Room.service.ts
@@ -1,21 +1,27 @@
 import fetch from "node-fetch";
 
+import logger from "@logger";
+
 import servicesConfig from "@configs/services.config";
 
 class RoomService {
     async findOneWithAuth(roomId: string, userId: string) {
-        const { room } = servicesConfig;
+        try {
+            const { room } = servicesConfig;
         
-        const response = await fetch(
-            `${room}/services-communication/room-service/room/with-auth?id=${roomId}&user_id=${userId}`
-        );
-
-        if (response.status === 200) {
-            const data = await response.json();
-            return data.body.room;
+            const response = await fetch(
+                `${room}/services-communication/room-service/room/with-auth?id=${roomId}&user_id=${userId}`
+            );
+    
+            if (response.status === 200) {
+                const data = await response.json();
+                return data.body.room;
+            }
+    
+            return null;
+        } catch (e) {
+            logger.log(`Error send to ChatService: ${e}`);
         }
-
-        return null;
     }
 }
 
